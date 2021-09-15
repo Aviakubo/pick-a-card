b7df20a03b0fd14ea7a11b2f111b4ee46cf41598
David Yakubov <dyakubov@Davids-MacBook-Air.local>
2021-09-15 11:41:07 -0400

    able to retrieve data for all three buttons but unable to retrieve a new deck ID afterwards

--- js/script.js
+++ js/script.js
@@ -1,28 +1,63 @@
-var myVariable;
+var deckID;
+var shuffle;
+var drawCard;
+
+// gets a new deck ID
+$.ajax({
+    url: 'https://deckofcardsapi.com/api/deck/new/'
+}).then(
+    (data) => {
+        deckID = data.deck_id;
+    },
+    (error) => {
+        console.log('bad request: ', error);
+    }
+);
+
+// sets deck ID so that it can be shuffled
+
+$('#shuffle').on('click', consoleLog2);
+
+function consoleLog2(event) {
+    event.preventDefault();
 
 $.ajax({
-        url: 'https://deckofcardsapi.com/api/deck/new/'
-    }).then(
-        (data) => { 
-        myVariable = data.deck_id;
-        },
-        (error) => {
+    url: `https://deckofcardsapi.com/api/deck/${deckID}/shuffle/`
+}).then(
+    (data) => {
+        shuffle = data.success;
+        console.log(shuffle);
+    },
+    (error) => {
         console.log('bad request: ', error);
-        }
-    );
-
-$('button').on('click', consoleLog);
-
-    function consoleLog(event) {
-        event.preventDefault();
-        console.log(myVariable);
     }
+);
+}
 
+// sets deck ID so that you can draw a card
 
-// generate new deck ID (does this delete old deck ID?)
+$('#draw').on('click', consoleLog3);
 
-// assign deck ID to variable
+function consoleLog3(event) {
+    event.preventDefault();
 
-// use variable to populate shuffle api link
+$.ajax({
+    url: `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`
+}).then(
+    (data) => {
+        drawCard = data.cards;
+        console.log(drawCard);
+    },
+    (error) => {
+        console.log('bad request: ', error);
+    }
+);
+}
 
-// 
+// pulls deck ID on click
+$('#open').on('click', consoleLog);
+
+function consoleLog(event) {
+    event.preventDefault();
+    console.log(deckID);
+}
