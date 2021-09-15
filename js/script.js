5fbaffef29dfc13eb19a5878879529ff8469389f
David Yakubov <dyakubov@Davids-MacBook-Air.local>
2021-09-15 11:22:04 -0400

    able to pull data and assign to variable

--- js/script.js
+++ js/script.js
@@ -1,12 +1,28 @@
-const promise = $.ajax({
-    url: 'https://deckofcardsapi.com/api/deck/new/'
-})
+var myVariable;
 
-promise.then(
-    (data) => {
-        console.log(data);
-    },
-    (error) => {
+$.ajax({
+        url: 'https://deckofcardsapi.com/api/deck/new/'
+    }).then(
+        (data) => { 
+        myVariable = data.deck_id;
+        },
+        (error) => {
         console.log('bad request: ', error);
+        }
+    );
+
+$('button').on('click', consoleLog);
+
+    function consoleLog(event) {
+        event.preventDefault();
+        console.log(myVariable);
     }
-);
+
+
+// generate new deck ID (does this delete old deck ID?)
+
+// assign deck ID to variable
+
+// use variable to populate shuffle api link
+
+// 
