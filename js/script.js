e4e8dd8596be61bd882ed2f4e36ec8d118675ecf
David Yakubov <dyakubov@Davids-MacBook-Air.local>
2021-09-15 13:14:42 -0400

    formatted and made minor changes to js but functionally, still the same

--- js/script.js
+++ js/script.js
@@ -1,28 +1,30 @@
+// VARIABLES
+// =========================================================
 var deckID;
 var shuffle;
 var drawCard;
 var remainder;
 
-// gets a new deck ID
+// GETS A NEW DECK
 // =========================================================
 $('#open').on('click', consoleLog);
 
 function consoleLog(event) {
     event.preventDefault();
-$.ajax({
-    url: 'https://deckofcardsapi.com/api/deck/new/'
-}).then(
-    (data) => {
-        deckID = data.deck_id;
-        console.log(deckID);
-    },
-    (error) => {
-        console.log('bad request: ', error);
-    }
-);
+    $.ajax({
+        url: 'https://deckofcardsapi.com/api/deck/new/'
+    }).then(
+        (data) => {
+            deckID = data.deck_id;
+            console.log(deckID);
+        },
+        (error) => {
+            console.log('bad request: ', error);
+        }
+    );
 }
 
-// sets deck ID so that it can be shuffled
+// SHUFFLES THE CARDS
 // =========================================================
 
 $('#shuffle').on('click', consoleLog2);
@@ -30,20 +32,22 @@ $('#shuffle').on('click', consoleLog2);
 function consoleLog2(event) {
     event.preventDefault();
 
-$.ajax({
-    url: `https://deckofcardsapi.com/api/deck/${deckID}/shuffle/`
-}).then(
-    (data) => {
-        shuffle = data.success;
-        console.log(shuffle);
-    },
-    (error) => {
-        console.log('bad request: ', error);
-    }
-);
+    $.ajax({
+        url: `https://deckofcardsapi.com/api/deck/${deckID}/shuffle/`
+    }).then(
+        (data) => {
+            shuffle = data;
+            console.log(`Was it shuffled? ${shuffle.shuffled}`);
+            console.log(`Was shuffle successful? ${shuffle.success}`);
+            console.log(shuffle.deck_id);
+        },
+        (error) => {
+            console.log('bad request: ', error);
+        }
+    );
 }
 
-// sets deck ID so that you can draw a card
+// DRAWS A RANDOM CARD
 // =========================================================
 
 $('#draw').on('click', consoleLog3);
@@ -51,17 +55,17 @@ $('#draw').on('click', consoleLog3);
 function consoleLog3(event) {
     event.preventDefault();
 
-$.ajax({
-    url: `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`
-}).then(
-    (data) => {
-        drawCard = data.cards;
-        remainder = data.remaining;
-        console.log(drawCard);
-        console.log(remainder);
-    },
-    (error) => {
-        console.log('bad request: ', error);
-    }
-);
+    $.ajax({
+        url: `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`
+    }).then(
+        (data) => {
+            drawCard = data.cards;
+            remainder = data.remaining;
+            console.log(drawCard);
+            console.log(remainder);
+        },
+        (error) => {
+            console.log('bad request: ', error);
+        }
+    );
 }
